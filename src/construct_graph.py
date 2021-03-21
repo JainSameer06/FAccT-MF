570126b6ad1d6e7e2a70d8b26538717ca18e1bff
Sameer Jain <jain.sameer96@gmail.com>
2021-03-21 22:41:23 +0530

    Complete script to generate citation network from the manually extracted FAccT citations

--- src/construct_graph.py
+++ src/construct_graph.py
@@ -1,33 +1,71 @@
 import networkx as nx
-import matplotlib.pyplot as plt
+import networkx.algorithms.community as nx_comm
 import csv
 import chart_studio.plotly as py
 from plotly.graph_objs import *
 from plotly.offline import iplot
-#from bokeh.io import output_file, show
-#from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine
-#from bokeh.plotting import figure, from_networkx
+import argparse
 
+#python3 construct_graph.py --s "/home/sameer/Projects/Political-leaning/Data/TXTs/titles.csv" --r "/home/sameer/Projects/Political-leaning/Data/TXTs/combined-final.csv" --f 0 --d "/home/sameer/Projects/Political-leaning/Data/TXTs/clusters.csv"
+
+def edge_to_remove(g):
+    d1 = nx.edge_betweenness_centrality(g, weight = "weight") 
+    list_of_tuples = list(d1.items()) 
+    
+      
+    sorted_list_of_tuples = sorted(list_of_tuples, key = lambda x:x[1], reverse = True) 
+    # Will return in the form (a,b) 
+    return sorted_list_of_tuples[0][0] 
+
+def girvan(g, k):
+    a = nx.connected_components(g) 
+    lena = len(list(a)) 
+    print (' The number of connected components is ', lena) 
+    while (lena < k): 
+  
+        # We need (a,b) instead of ((a,b)) 
+        u, v = edge_to_remove(g) 
+        g.remove_edge(u, v)  
+          
+        a = nx.connected_components(g) 
+        lena = len(list(a)) 
+        print (' The number of connected components is ', lena) 
+    a = list(sorted(nx.connected_components(g), key = len, reverse = True))
+    components_sorted = []
+    for component in a:
+    	sources = [node for node in component if int(node) < 1000]
+    	sources = sorted(sources, key = g.degree, reverse = True)
+    	destinations = [node for node in component if int(node) >= 1000]
+    	destinations = sorted(destinations, key = g.degree, reverse = True)
+    	components_sorted.append(sources + destinations)
+    #print(components_sorted)
+    return components_sorted
 
 def generate_node(record):
 	node_attributes = {}
 	node_attributes["id"] = record[1] 
 
-
 def get_title_dictionary(title_file):
-	with open("/home/sameer/Projects/Political-leaning/Data/TXTs/titles.csv") as titlefile:
+	with open(title_file) as titlefile:
 		reader = csv.reader(titlefile)
 		title_dict = {}
 		for row in reader:
 			title_dict[row[0]] = row[1]
 	return title_dict
 
-G = nx.DiGraph()
+def get_node_text(G):
+	reference_articles_titles = [G.nodes[node]['articleTitle'] if 'articleTitle' in G.nodes[node] else 'none' for node in G.nodes()]
+	reference_articles_authors = [G.nodes[node]['authors'] if 'authors' in G.nodes[node] else 'none' for node in G.nodes()]
+	node_text = ['Title: {title}\nAuthors: {author}'.format(title = title, author = author) for title, author in zip(reference_articles_titles, reference_articles_authors)]
+	return node_text
 
-with open("/home/sameer/Projects/Political-leaning/Data/TXTs/combined-final.csv") as csvfile:	
-	title_dict = get_title_dictionary("dummy")
-	reader = csv.reader(csvfile)
+def construct_graph(title_file, reference_csv, facct_flag):
+	G = nx.Graph()
+	title_dict = get_title_dictionary(title_file)
+	reader = csv.reader(reference_csv)
+	prev_ref_id = -1
 	for row in reader:
+		add_flag = 0
 		if row[1] == "Id":
 			continue
 		source_id = row[1]
@@ -36,33 +74,33 @@ with open("/home/sameer/Projects/Political-leaning/Data/TXTs/combined-final.csv"
 		if source_id not in G:
 			source_filename = row[2]
 			source_title = title_dict[source_filename]
-			G.add_node(source_id, sourceArticle = source_title)
+			G.add_node(source_id, articleTitle = source_title)
 
-		if reference_id not in G:
-			G.add_node(reference_id, referencedArticle = row[4], authors = row[3], venue = row[5], year = row[6])
+		if (int(reference_id) < 1000) or (int(reference_id) >= 1000 and reference_id == prev_ref_id):
+			add_flag = 1
+		if reference_id not in G and add_flag == 1:
+			G.add_node(reference_id, articleTitle = row[4], authors = row[3], venue = row[5], year = row[6])
+		prev_ref_id = reference_id
 			
-		G.add_edge(source_id, reference_id)
+		if add_flag == 1:
+			if int(reference_id) < 1000:
+				G.add_edge(source_id, reference_id, weight = 1)
+			else:
+				G.add_edge(source_id, reference_id, weight = 1)
+	return G
 
+def plot_graph(G):
 	pos = nx.fruchterman_reingold_layout(G)		#returns a dictionary of positions keyed by node
 	Xv = [pos[k][0] for k in G.nodes()]
 	Yv = [pos[k][1] for k in G.nodes()]
 	Xed = []
 	Yed = []
 
-	reference_articles_titles = [G.nodes[node]['referencedArticle'] if 'referencedArticle' in G.nodes[node] else 'none' for node in G.nodes()]
-	reference_articles_authors = [G.nodes[node]['authors'] if 'authors' in G.nodes[node] else 'none' for node in G.nodes()]
-	node_text = ['Title: {title}\nAuthors: {author}'.format(title = title, author = author) for title, author in zip(reference_articles_titles, reference_articles_authors)]
+	node_text = get_node_text(G)
+	
 	for edge in G.edges():
 	    Xed += [pos[edge[0]][0], pos[edge[1]][0], None]
 	    Yed += [pos[edge[0]][1], pos[edge[1]][1], None]
-
-	node_adjacencies = []
-	
-	#for node, adjacencies in enumerate(G.adjacency()):
-	    #node_adjacencies.append(len(adjacencies[1]))
-	    #node_text.append('# of connections: '+str(len(adjacencies[1])))
-
-
 
 	edge_trace = Scatter(x=Xed,
 	               y=Yed,
@@ -85,51 +123,29 @@ with open("/home/sameer/Projects/Political-leaning/Data/TXTs/combined-final.csv"
 	               )
 
 	node_trace.marker.size = [in_deg[1] for in_deg in G.in_degree()]
-	annot="This networkx.Graph has the Fruchterman-Reingold layout<br>Code:"+\
-	"<a href='http://nbviewer.ipython.org/gist/empet/07ea33b2e4e0b84193bd'> [2]</a>"
 
 	data1 = [edge_trace, node_trace]
-	fig1 = Figure(data=data1, layout=None)
-	#fig1['layout']['annotations'][0]['text']=annot
+	fig1 = Figure(data = data1, layout = None)
 	iplot(fig1, filename = 'citation-network')
 
-
-
-	"""
-	d = dict(G.in_degree)
-
-	nx.draw(G, nodelist=list(d.keys()), node_size=[v * 100 for v in d.values()])
-	plt.show()
-
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--s", dest = 'sourceTitleFile', type = str, help = "Path to the file mapping source filenames to article titles")
+	parser.add_argument("--r", dest = 'referenceFile', type = str, help = "Path to the reference file")
+	parser.add_argument("--d", dest = 'destinationFile', type = str, help = "Path to the destination file")
+	parser.add_argument("--f", dest = 'facctFlag', type = int, help = "Boolean set to 1 if graph is to include only FACCT papers")
+	parser.add_argument("--k", dest = 'numClusters', type = int, help = "Number of clusters")
 	
-	plot = figure(tools="pan, wheel_zoom, save, reset", active_scroll='wheel_zoom',
-            x_range=Range1d(-100.1, 100.1), y_range=Range1d(-100.1, 100.1), title="Citation")
+	args = parser.parse_args()
+	destinationFile = args.destinationFile + "_" + str(args.numClusters) + ".csv"
 
-	
-
-	#Set node size and color
-	degrees = dict(nx.degree(G))
-	nx.set_node_attributes(G, name='degree', values = degrees)
-	print(degrees)
-
-	number_to_adjust_by = 2
-	adjusted_node_size = dict([(node, degree + number_to_adjust_by) for node, degree in nx.degree(G)])
-	nx.set_node_attributes(G, name='adjusted_node_size', values = adjusted_node_size)
-
-	network_graph = from_networkx(G, nx.spring_layout, scale=100, center=(0, 0))
-	size_by_this_attribute = 'adjusted_node_size'
-	#network_graph.node_renderer.data_source.data['index'] = list(reversed(range(len(G))))
-	network_graph.node_renderer.glyph = Circle(size=size_by_this_attribute, fill_color='skyblue')
-
-	
-
-	#Set edge opacity and width
-	network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.02, line_width=1)
-
-	#Add network graph to the plot
-	plot.renderers.append(network_graph)
-
-	show(plot)
-	#save(plot, filename=f"{title}.html")
-	"""
-
+	with open(args.referenceFile) as csvfile:	
+		G = construct_graph(args.sourceTitleFile, csvfile, args.facctFlag)
+		component_list = girvan(G, args.numClusters) 
+		with open(destinationFile, 'w', newline='') as csvfile:
+			csv_writer = csv.writer(csvfile)
+			for component in component_list: 
+			    for node in component:
+			    	csv_writer.writerow([node, G.nodes[node]['articleTitle'], G.degree[node]])
+			    csv_writer.writerow(['.............', '............']) 
+			print(nx_comm.modularity(G, component_list))
